91630d2d1ef33e482d0b79e58c15d3cb25bc9c74
Liviu Ionescu <ilg@livius.net>
2015-11-09 22:57:45 +0200

    reindent with 4s

--- bin/xcdl-js-cli.js
+++ bin/xcdl-js-cli.js
@@ -1,23 +1,25 @@
 #!/usr/bin/env node
 ;
-(function() { // wrapper in case we're in module_context mode
+(function () { // wrapper in case we're in module_context mode
 
-  app_name = 'xcdl-js';
+    app_name = 'xcdl-js';
 
-  // windows: running "xcdl blah" in this folder will invoke WSH, not node.
-  /* global WScript */
-  if (typeof WScript !== 'undefined') {
-    WScript.echo(app_name + ' does not work when run\n'
-        + 'with the Windows Scripting Host\n\n'
-        + "'cd' to a different folder,\n" + "or type '" + app_name
-        + ".cmd <args>',\n" + "or type 'node " + app_name + " <args>'.");
-    WScript.quit(1);
-    return;
-  }
+    // windows: running "xcdl blah" in this folder will invoke WSH, not node.
+    /* global WScript */
+    if (typeof WScript !== 'undefined') {
+        WScript
+                .echo(app_name + ' does not work when run\n'
+                        + 'with the Windows Scripting Host\n\n'
+                        + "'cd' to a different folder,\n" + "or type '"
+                        + app_name + ".cmd <args>',\n" + "or type 'node "
+                        + app_name + " <args>'.");
+        WScript.quit(1);
+        return;
+    }
 
-  process.title = app_name;
-  var app = require('../lib/main.js');
+    process.title = app_name;
+    var app = require('../lib/main.js');
 
-  app.run(process.argv.slice(2));
-  
+    app.run(process.argv.slice(2));
+
 })()
