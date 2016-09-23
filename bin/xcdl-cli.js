5e4d03ec7227233647a7b0d52ca9bd197d17f572
Liviu Ionescu <ilg@livius.net>
2016-09-23 21:00:44 +0300

    restructured; interactive mode added, via repl
    - files renamed
    - commands are a single file, not a folder

--- bin/xcdl-cli.js
+++ bin/xcdl-cli.js
@@ -1,16 +1,136 @@
 #!/usr/bin/env node
 
-(function () { // wrapper in case we're in module_context mode
+/*
+ * This file is part of the XCDL distribution 
+ *   (http://xcdl.github.io).
+ * Copyright (c) 2015 Liviu Ionescu.
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
 
-    app_name = 'xcdl';
+// When installed globally, several links from system locations
+// (like /usr/local/bin/xcdl, /usr/local/bin/xpack)
+// are created to point to this script, so this script may be
+// invoked with different names.
 
-    // On Windows: running 'xcdl blah' in this folder will invoke WSH, not node.
-    var wsa = require('wscript-avoider')
-    wsa.quit_if_wscript(app_name)
+// Wrapper in case we're in module_context mode.
+// Prevent the '(' being interpreted as a function call,
+// define an anonymous function and execute it.
+; (function () {
 
-    process.title = app_name;
-    var app = require('../lib/main.js')
+  'use strict'
 
-    app.run(process.argv.slice(2))
+  // --------------------------------------------------------------------------  
+  // Dependencies.  
+  const avoider = require('wscript-avoider')
+  const path = require('path')
+  const vm = require('vm')
 
-}());
+  const xcdl = require('../lib/xcdl.js')
+
+  // Conditional dependencies (listed here for completeness).
+  // const repl = require('repl')
+
+  // --------------------------------------------------------------------------    
+  // Application name.  
+  const appName = 'xcdl'
+
+  // Avoid running on WScript.
+  avoider.quit_if_wscript(appName)
+
+  // Replace 'node' with the application name, to help `ps` show
+  // a more accurate situation.
+  process.title = appName
+
+  const log = require('npmlog')
+  log.pause() // will be unpaused after config is loaded.
+  log.disableColor()
+
+  log.info('it worked if it ends with', 'ok')
+  log.info(`argv0: ${process.argv[1]}`)
+
+
+  // argv[0] = '/usr/local/bin/node'
+  // argv[1] = '/.../xcdl-js.git/bin/xcdl-cli.js' or '/.../bin/xpack'
+  // Skip the first two strings and keep only the specific ones.
+  let args = process.argv.slice(2)
+
+  // Als o used as prompt in interractive mode.  
+  let cmd = path.basename(process.argv[1])
+
+  if (cmd === 'xpack') {
+    // Transform 'xpack a b c' -> 'xcdl pack a b c'
+    args.unshift('pack')
+  } else if (cmd === 'xrepo') {
+    // Transform 'xrepo a b c' -> 'xcdl repo a b c'
+    args.unshift('repo')
+  } else {
+    cmd = 'xcdl'
+  }
+
+  log.level = 'verbose'
+  log.resume()
+
+  if (xcdl.isOneShot(process.argv, cmd)) {
+    // Single shot invocation.
+
+    //xcdl.run(args)
+    let ctx = vm.createContext()
+
+    // This is the only mandatory field in the context, so far.    
+    ctx.console = console
+
+    // The actual command name is passed via a custom context field.
+    ctx.cmdName = cmd
+
+    // The process arguments are passed via a custom context field.
+    ctx.processArgv = args
+
+    ctx.isBatch = true    
+    let cmdLine = args.join(' ')
+    let filename = null
+    xcdl.run(cmdLine, ctx, filename, (er) => {
+      if (er) {
+        console.error('Error:', er.message)
+      }
+    })
+  } else {
+    // Interractive mode.
+
+    // TODO: get rid of the stack trace.    
+    const d = require('domain').create();
+    d.on('error', (er) => {
+      console.error('ErroR:', er.message)
+    })
+
+    const repl = require('repl')
+    const replServer = repl.start(
+      {
+        prompt: cmd + '> '
+        , eval: xcdl.run
+        , completer: xcdl.completer
+        , domain: d
+      })
+
+  }
+
+} ())
